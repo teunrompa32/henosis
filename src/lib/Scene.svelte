0e8585f299f4019c0cb781a95148ec4f484491a9
teunrompa32 <teunrompa@gmail.com>
2025-03-26 13:20:49 +0100

    initial commit

---
+++ src/lib/Scene.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {T, useTask, useLoader} from  '@threlte/core';
+    import {interactivity} from '@threlte/extras';
+    import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+    const gltf = useLoader(GLTFLoader).load('/models/logo_3d.glb');
+
+    let rotation = 0;
+
+    useTask((delta) =>{
+        rotation += delta;
+    })
+
+    interactivity()
+</script>
+
+{#if $gltf}
+    <T scale={[30,30,30]} rotation.y={rotation} is={$gltf.scene} />
+{/if}
