8c0557c8f97ff11a31934dd9f5baf1216271b811
teunrompa32 <teunrompa@gmail.com>
2025-03-27 17:40:48 +0100

    implemented material updating

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -1,9 +1,32 @@
-<script>
+<script lang='ts'>
     import {T, useTask, useLoader} from  '@threlte/core';
     import {interactivity} from '@threlte/extras';
-    import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+    import {GLTFLoader, type GLTF} from 'three/examples/jsm/loaders/GLTFLoader.js';
+    import {AmbientLight, Color, MeshStandardMaterial} from 'three';
 
     const gltf = useLoader(GLTFLoader).load('/models/logo_3d.glb');
+
+    let updatedMaterial: MeshStandardMaterial;
+
+    function setMaterial(model: GLTF): void{
+        const scene = model.scene;
+        const mesh = scene.children[0];
+
+        mesh.material = new MeshStandardMaterial({
+            color: new Color(220,23,0),
+            roughness: 0.5,
+            metalness: 0.5
+        });
+
+        updatedMaterial = mesh.material;
+        console.log("mesh.material", mesh.material);
+    }
+
+    gltf.then((model) => {{
+        setMaterial(model);
+    }})   
+    
+
 
     let rotation = 0;
 
@@ -15,5 +38,18 @@
 </script>
 
 {#if $gltf}
-    <T scale={[30,30,30]} rotation.y={rotation} is={$gltf.scene} />
+    <T.Group
+        scale={[10, 10, 10]}
+    >
+    <T.AmbientLight ></T.AmbientLight>
+    <T.DirectionalLight position={[5,5,5]}/>
+        <T.Mesh 
+        castShadow 
+        receiveShadow
+        rotation={[0, rotation, 0]}
+        geometry={$gltf.scene.children[0].geometry}
+        material={updatedMaterial}
+        position={[0, 0, 0]}
+        ></T.Mesh>
+    </T.Group>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,20 @@
     import Scene  from '$lib/Scene.svelte';
 </script>
 
-<Canvas>
+<div class='canvas'>
+<Canvas >
     <Scene/>
 </Canvas>
+</div>
 
+
+
+<style>
+    .canvas {
+        width: 100%;
+        height: 100%;
+        position: 'absolute';
+        background-color: gradient(180deg, #000000 0%, #000000 100%);
+
+    }
+</style>
